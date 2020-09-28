852121ec0de69263d693ce9d4162462449973c57
Victor <golube4754@gmail.com>
2020-09-28 21:59:57 +0300

    first commit

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace GolubevV
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
